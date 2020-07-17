af94f4b54cebe1150b1b52bb1b09dd043082999a
AdrienM <privateadrieps@gmail.com>
2020-07-17 11:44:24 +0200

    v0.4.1 : Affichage du dialoguebox pour demander le numéro des cartes

--- app/src/main/java/com/example/fresquerappel/ui/recherche/RechercheDialogueFragment.kt
+++ app/src/main/java/com/example/fresquerappel/ui/recherche/RechercheDialogueFragment.kt
@@ -1,6 +1,7 @@
 package com.example.fresquerappel.ui.recherche
 
 import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -8,10 +9,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.EditorInfo
+import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
-import android.widget.TextView.OnEditorActionListener
-import androidx.core.widget.addTextChangedListener
+import android.widget.EditText
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.DialogFragment
 import com.example.fresquerappel.R
 import kotlinx.android.synthetic.main.dialogue_fragment_recherche.*
@@ -19,21 +20,9 @@ import kotlinx.android.synthetic.main.dialogue_fragment_recherche.*
 
 class RechercheDialogueFragment : DialogFragment() {
 
-    class rechTextWatcher : TextWatcher {
-        override fun afterTextChanged(s: Editable?) {
-            Log.d("test", "Nous sommes APRES le textChanged")
-        }
-
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            Log.d("test", "Nous sommes AVANT le textChanged")
-        }
-
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            //if(ETcarte1.text.length)
-            Log.d("test", "Nous sommes dans le textChanged")
-        }
+    companion object{
+        val maxLengthNumber = 2
     }
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -48,17 +37,98 @@ class RechercheDialogueFragment : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //récupère le focus
-        ETcarte1.requestFocus()
+
+        ETcarte1.addTextChangedListener(TextWatcherGoTo(ETcarte2))
+
+        /*
+        ETcarte1.setFocusable(true)
+        ETcarte1.setFocusableInTouchMode(true)
+
+        ETcarte2.setFocusable(true)
+        ETcarte2.setFocusableInTouchMode(true)
+
 
         //Set le focus. Je dois passer par l'activité pour pouvoir donné la string du Systeme service demandé
         //Système service = service présent de base dans Android
+        /*val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        imm?.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY) */
+
+
+
         val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        //imm?.showSoftInput(ETcarte1, InputMethodManager.SHOW_IMPLICIT)
         imm?.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY)
+        //Log.d("test", "b4")
 
-        Log.d("test", "b4")
 
-        ETcarte1.addTextChangedListener(rechTextWatcher())
-
+        //récupère le focus
+        //ETcarte1.requestFocus()
+        */
     }
+
+    /*
+    override fun onCancel(dialog: DialogInterface) {
+        super.onCancel(dialog)
+
+        Log.d("test", "cancel")
+
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        //imm?.hideSoftInputFromWindow(view?.windowToken, 0)
+        //imm?.hideSoftInputFromWindow(view?.windowToken, 0)
+        imm?.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, InputMethodManager.HIDE_IMPLICIT_ONLY)
+        imm?.hideSoftInputFromWindow(view?.windowToken, 1)
+    }
+*/
+
+    /*
+    override fun onDestroyView() {
+
+
+        Log.d("test", "destroy")
+
+        //val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        //imm?.hideSoftInputFromWindow(view?.windowToken, 0)
+        //imm?.hideSoftInputFromWindow(view?.windowToken, 0)
+        //imm?.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, InputMethodManager.HIDE_IMPLICIT_ONLY);
+
+        super.onDestroyView()
+    }
+*/
+
+    //Text watchers : actions au moment où l'utilisateur rentre le texte. Passe la main à l'edittext suivant au moment où la taille maximale est atteinte
+    class TextWatcherGoTo(val editTextGoTo: EditText) : TextWatcher {
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            //if(ETcarte1.text.length)
+            if(s?.length!! >= maxLengthNumber) {
+                //Log.d("test", "2")
+                editTextGoTo.requestFocus()
+            }
+        }
+
+        //Fonction non utilisées
+        override fun afterTextChanged(s: Editable?) {
+            //Log.d("test", "Nous sommes APRES le textChanged "+ s?.length + " " + s.toString())
+        }
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            //Log.d("test", "Nous sommes AVANT le textChanged" + s.toString() + start + " " + count + " " + after)
+        }
+    }
+
+
+    class TextWatcherStartSearch : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+            Log.d("test", "Nous sommes APRES le textChanged")
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            Log.d("test", "Nous sommes AVANT le textChanged")
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            //if(ETcarte1.text.length)
+            Log.d("test", "Nous sommes dans le textChanged")
+        }
+    }
+
 }
